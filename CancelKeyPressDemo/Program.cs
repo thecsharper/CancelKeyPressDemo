abb090c671218d1e80bcac382731a9ed0565e987
thecsharper <si@thecsharper.com>
2023-03-29 15:55:28 +0100

    Add project files.

---
+++ CancelKeyPressDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// https://www.meziantou.net/handling-cancelkeypress-using-a-cancellationtoken.htm
+using var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (sender, e) =>
+{
+    e.Cancel = true;
+    cts.Cancel();
+};
+
+await MainAsync(args, cts.Token);
+
+static async Task MainAsync(string[] args, CancellationToken cancellationToken)
+{
+    try
+    {
+        while (true)
+        {
+            Console.WriteLine("Waiting..");
+            await Task.Delay(10_000, cancellationToken);
+        }
+    }
+    catch (OperationCanceledException)
+    {
+        Console.WriteLine("Operation canceled");
+    }
+}
